86fd36fff38529189046a85d3a01a9e20733ec8f
RomaVintonyak <19roman23@gmail.com>
2020-03-05 16:21:48 +0200

    modified portfolio script

--- js/block/portfolio__tab.js
+++ js/block/portfolio__tab.js
@@ -16,5 +16,26 @@ jQuery(document).ready(function() {
         }
       });
     }
+    var pbH = $(".portfolio__box");
+    pbH.each(function() {
+      var wWidth = $(window).width();
+      var marginH;
+      if (wWidth < 768) {
+        marginH = -200;
+      } else if (wWidth < 1024) {
+        marginH = -150;
+      } else {
+        marginH = -100;
+      }
+      if ($(this).hasClass("hide")) {
+        $(this).css({
+          "margin-top": marginH + "%"
+        });
+      } else {
+        $(this).css({
+          "margin-top": "0%"
+        });
+      }
+    });
   });
 });
