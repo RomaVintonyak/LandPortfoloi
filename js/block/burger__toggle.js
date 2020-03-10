2a11236ceb2f8d1fba5e7f2807caf020ea948d19
RomaVintonyak <19roman23@gmail.com>
2020-03-10 14:57:54 +0200

    finished burger button

--- js/block/burger__toggle.js
+++ js/block/burger__toggle.js
@@ -1,10 +1,12 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
   "use script";
   var burgerBtn = $("#burgerToggle");
   var navMenu = $(".mobile__menu");
-  burgerBtn.on("click", function(event){
+  burgerBtn.on("click", function(event) {
     event.preventDefault();
-    
+    $(this)
+      .find("span")
+      .toggleClass("burger__open");
     navMenu.toggleClass("mobile__menu--open");
   });
 });
